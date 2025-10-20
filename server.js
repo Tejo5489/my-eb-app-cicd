53849901d2078f149f1dbe72c3f2e21d95fb38c0
Tejo Chandra Kantheti <156089495+Tejo5489@users.noreply.github.com>
2025-10-20 23:06:52 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+// A simple Node.js server to serve static content
+const http = require('http');
+const fs = require('fs');
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  fs.readFile('index.html', (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(data);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
